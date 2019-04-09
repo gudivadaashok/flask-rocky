15c657394fdc9c05df4c8a85852a700c2c34f77f
Ashok Gudivada <ashok.gudivada@eagles.oc.edu>
2019-04-09 08:50:10 -0400

    del setup.py

--- features/steps/steps.py
+++
@@ -1,9 +0,0 @@
-from behave import *
-
-@when(u'I navigate to Home Page')
-def step_impl(ctx):
-    ctx.resp = ctx.client.get('/')
-
-@then(u'Home Page should be displayed')
-def step_impl(ctx):
-    assert 'This is the home page' in ctx.resp
